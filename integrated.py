d7d88d13504fc38e1aca3eb08f189a17e6c5c02a
Tzu Chiang <fs0000016@gmail.com>
2022-01-16 16:55:45 +0800

    2022/1/16 增加了偵測有人沒在賺錢就會打電話的功能。

--- integrated.py
+++ integrated.py
@@ -1,7 +1,10 @@
 import monitor
 import alarm
+import time
 
-a = monitor.check()   # 沒在賺錢的角色數量。
-print(a)
-if a > 0:
-    alarm.call()     # 打電話
+while True:
+    a = monitor.check()   # 沒在賺錢的角色數量。
+    print(a)
+    if a > 0:
+        alarm.call()     # 打電話
+    time.sleep(1800)
