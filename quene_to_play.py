ace65900dd72ca42d912c15ec029c02755246418
Tzu Chiang <fs0000016@gmail.com>
2022-01-23 23:31:55 +0800

    2022/1/23 點滑鼠的部份改用pydirectinput           所顯示的訊息都加上時間           排隊的訊息加上時間

--- quene_to_play.py
+++ quene_to_play.py
@@ -1,20 +1,21 @@
 import pyautogui
 import time
 import alarm
+import time_recorded
 
-print("程式將於5秒後開始偵測，請勿讓本視窗擋住要偵測的畫面")
+print("程式將於5秒後開始偵測，請勿讓本視窗擋住要偵測的畫面     "+ time_recorded.time_message())
 time.sleep(5)
 number = len(list(pyautogui.locateAllOnScreen(r'for_quene/quene.PNG')))
-print(f'偵測到{number}個人物排隊中')
+print(f'偵測到{number}個人物排隊中     '+ time_recorded.time_message())
 
 time_start = time.time()
 
 while len(list(pyautogui.locateAllOnScreen(r'for_quene/quene.PNG'))) == number:
-    print(f'程式執行已經過{round(time.time()-time_start)}秒鐘')
+    print(f'程式偵測已經過{round(time.time()-time_start)}秒鐘     '+ time_recorded.time_message())
     time.sleep(60)
-print("呼叫撥電話程式中")
+print("呼叫撥電話程式中     " + time_recorded.time_message())
 alarm.call()
-print("程式將於5秒後自動關閉")
+print("程式將於5秒後自動關閉     " + time_recorded.time_message())
 time.sleep(5)
 # pyautogui.click(pyautogui.locateCenterOnScreen(r'for_quene/call.PNG'))
 
