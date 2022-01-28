05b34dc8a65ce78e7d46ff76fb460c21a9f75c5d
Tzu Chiang <fs0000016@gmail.com>
2022-01-28 13:26:33 +0800

    2022/1/28  弄了一個排到隊伍後傳簡訊的版本 2022/1/27  把偵測角色是否在賺錢的通知方法改成發skype簡訊            調整alarm，把偵測不到打電話圖示的exception改掉 2022/1/24  弄一個py檔把所有功能整合在一個執行檔

--- quene_to_play.py
+++ quene_to_play.py
@@ -2,6 +2,7 @@ import pyautogui
 import time
 import alarm
 import time_recorded
+import send_skype_message
 
 def quene():
     print("程式將於5秒後開始偵測，請勿讓本視窗擋住要偵測的畫面     "+ time_recorded.time_message())
@@ -18,8 +19,9 @@ def quene():
         while len(list(pyautogui.locateAllOnScreen(r'for_quene/quene.PNG'))) == number:
             print(f'程式偵測已經過{round(time.time()-time_start)}秒鐘     '+ time_recorded.time_message())
             time.sleep(60)
-        print("呼叫撥電話程式中     " + time_recorded.time_message())
-        alarm.call()
+        # print("呼叫撥電話程式中     " + time_recorded.time_message())
+        # alarm.call()
+        send_skype_message.send_message(999,999)
         print("程式將於5秒後自動關閉     " + time_recorded.time_message())
         time.sleep(5)
         # pyautogui.click(pyautogui.locateCenterOnScreen(r'for_quene/call.PNG'))
