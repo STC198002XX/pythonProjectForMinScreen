a79e7eac03f6e346758ff99aa57217784caa11d2
Tzu Chiang <fs0000016@gmail.com>
2022-04-24 10:06:40 +0800

    2022/1/28  弄了一個排到隊伍後傳簡訊的版本 2022/1/27  把偵測角色是否在賺錢的通知方法改成發skype簡訊            調整alarm，把偵測不到打電話圖示的exception改掉 2022/1/24  弄一個py檔把所有功能整合在一個執行檔 2022/4/24  change the skype contacts to Mr. Ro 2022/4/24  change the skype contacts to Mr. Ro

--- send_skype_message.py
+++ send_skype_message.py
@@ -6,7 +6,7 @@ def send_message(a, n):
     contacts = sk.contacts # sk的聯絡人
     for i in contacts:  # 把聯絡人都列出來，可以查到他們的ID
         print(i)
-    aaa = sk.contacts['live:lspropane']  # 聯絡人物件
+    aaa = sk.contacts['live:.cid.c0e9aaf274fc86e4']  # 聯絡人物件
     aaa.chat.sendMsg(f"第{n}次連續警報:有{a}個人沒在賺錢     "+time_recorded.time_message())    # 送簡訊給這個聯絡人
 
 if __name__=="__main__":
