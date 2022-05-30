6568eacf89420a4d3201af57431901adb1d5cfa7
Tzu Chiang <fs0000016@gmail.com>
2022-05-30 19:57:26 +0800

    2022/5/31 結合賣拍賣場及買武卷

--- arduino_restock_all_the_account.py
+++ arduino_restock_all_the_account.py
@@ -1,9 +1,12 @@
 import pyautogui
 import restock_auction
-all_account_position = pyautogui.locateAllOnScreen(r'for_arduino\square.png')
-for position in all_account_position:
-    x = position[0]+position[2]//2
-    y = position[1]+position[3]//2
-    # print(f'{position}  x:{x}  y:{y}')
-# for x, y in all_account_position:
-    restock_auction.restock_the_good_function(x, y)
+
+
+def daily_sell_in_the_auction():
+    all_account_position = pyautogui.locateAllOnScreen(r'for_arduino\square.png')
+    for position in all_account_position:
+        x = position[0]+position[2]//2
+        y = position[1]+position[3]//2
+        # print(f'{position}  x:{x}  y:{y}')
+    # for x, y in all_account_position:
+        restock_auction.restock_the_good_function(x, y)
---
+++ daily_buy_scrolls_and_sell_in_auction.py
@@ -0,0 +1,4 @@
+import arduino_buy_the_enhancing_scroll
+import arduino_restock_all_the_account
+arduino_buy_the_enhancing_scroll.daily_but_the_scrolls()
+arduino_restock_all_the_account.daily_sell_in_the_auction()
