0388adeb93afe5eebf9b4f0bdb872c33d6faf06f
Tzu Chiang <fs0000016@gmail.com>
2022-06-27 21:51:17 +0800

    2022/6/26 偵測人物賺錢狀況及拍賣、買武防卷合併

--- daily_buy_scrolls_and_sell_in_auction.py
+++ daily_buy_scrolls_and_sell_in_auction.py
@@ -3,3 +3,6 @@ import arduino_restock_all_the_account
 def do_all():
     arduino_buy_the_enhancing_scroll.daily_but_the_scrolls()
     arduino_restock_all_the_account.daily_sell_in_the_auction()
+
+if __name__ == "__main__":
+    do_all()
